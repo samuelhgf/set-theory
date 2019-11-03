f4255d037d8942149f72512416f6ff32788feb92
Rodolfo Marinho <rodolfo@grupomarinho.com.br>
2019-11-03 18:49:49 -0300

    Add BinaryRelationUtils class

---
+++ equivalence-relation/projeto-logica-unifacisa/BinaryRelationUtils.py
@@ -0,0 +1,65 @@
+#coding: utf-8
+
+
+class BinaryRelationUtils(object):
+    """Class providing utilities to verify properties of a binary relation."""
+
+    @staticmethod
+    def verify_reflexivity(binary_relation, input_set):
+        """
+        This method verifies if a given binary relation is reflexive or not.
+
+        Arguments:
+        binary_relation - A subclass of the BinaryRelation class.
+
+        input_set - A set closed under the binary relation.
+
+        Return True if the binary relation in the given input set is reflexive
+        or False if it is not.
+        """
+        pass
+
+    @staticmethod
+    def verify_symmetry(binary_relation, input_set):
+        """
+        This method verifies if a given binary relation is symmetric or not.
+
+        Arguments:
+        binary_relation - A subclass of the BinaryRelation class.
+
+        input_set - A set closed under the binary relation.
+
+        Return True if the binary relation in the given input set is symmetric
+        or False if it is not.
+        """
+        pass
+
+    @staticmethod
+    def verify_transitivity(binary_relation, input_set):
+        """
+        This method verifies if a given binary relation is transitive or not.
+
+        Arguments:
+        binary_relation - A subclass of the BinaryRelation class.
+
+        input_set - A set closed under the binary relation.
+
+        Return True if the binary relation in the given input set is transitive
+        or False if it is not.
+        """
+        pass
+
+    @staticmethod
+    def verify_antisymmetry(binary_relation, input_set):
+        """
+        This method verifies if a given binary relation is antisymmetric or not.
+
+        Arguments:
+        binary_relation - A subclass of the BinaryRelation class.
+
+        input_set - A set closed under the binary relation.
+
+        Return True if the binary relation in the given input set is 
+        antisymmetric or False if it is not.
+        """
+        pass
